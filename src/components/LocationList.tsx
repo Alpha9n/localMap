d6f0e093aa3e2f5a04ec173d750b69b98655aaaa
Alpha9n <42644369+Alpha9n@users.noreply.github.com>
2024-07-03 18:37:34 +0900

    fix: 相対サイズを使用するように変更

--- src/components/LocationList.tsx
+++ src/components/LocationList.tsx
@@ -29,54 +29,54 @@ export const LocationCard = ({title, description, link, imgLink, latLng, locButt
     const {value, setValueAndStorage} = useLocalStorage(title, 'false');
 
     return (
-        <Card className="w-full min-w-[350px] h-[150px]">
+        <Card className="w-full min-w-[20rem] h-[150px]">
             <CardHeader className="justify-between">
-            <div className="flex gap-5">
-                <Avatar isBordered radius="sm" size="md" src={imgLink} />
-                <div className="flex flex-col gap-1 items-start justify-center">
-                    <h4 className="text-small font-semibold leading-none text-default-600">{title}</h4>
+                <div className="flex gap-5">
+                    <Avatar isBordered radius="sm" size="md" src={imgLink} />
+                    <div className="flex flex-col gap-1 items-start justify-center">
+                        <h4 className="text-small font-semibold leading-none text-default-600">{title}</h4>
+                    </div>
                 </div>
-            </div>
-            <div
-                className="flex gap-2"
-            >
-                <Button
-                    color="primary"
-                    radius="full"
-                    size="md"
-                    onPress={() => {
-                        locButtonCallback(latLng, title, description, link, imgLink);
-                    }}
+                <div
+                    className="flex gap-2"
                 >
-                    <IoMdPin/>
-                </Button>
-                <Button
-                    className={value === 'true' ? "bg-transparent text-foreground border-default-200" : ""}
-                    color="primary"
-                    radius="full"
-                    size="md"
-                    variant={value === 'true' ? "bordered" : "solid"}
-                    onPress={() => {
-                        setValueAndStorage(value==='true' ? 'false' : 'true');
-                    }}
-                    isIconOnly
-                >
-                    {value === 'true' ? <FaStar /> : <FaRegStar /> }
-                </Button>
-            </div>
+                    <Button
+                        color="primary"
+                        radius="full"
+                        size="md"
+                        onPress={() => {
+                            locButtonCallback(latLng, title, description, link, imgLink);
+                        }}
+                    >
+                        <IoMdPin/>
+                    </Button>
+                    <Button
+                        className={value === 'true' ? "bg-transparent text-foreground border-default-200" : ""}
+                        color="primary"
+                        radius="full"
+                        size="md"
+                        variant={value === 'true' ? "bordered" : "solid"}
+                        onPress={() => {
+                            setValueAndStorage(value === 'true' ? 'false' : 'true');
+                        }}
+                        isIconOnly
+                    >
+                        { value === 'true' ? <FaStar /> : <FaRegStar /> }
+                    </Button>
+                </div>
             </CardHeader>
             <CardBody className="px-3 py-0 text-small text-default-400">
-            <p>{description}</p>
+                <p>{description}</p>
             </CardBody>
             <CardFooter className="gap-5">
-            <div className="flex gap-1">
-                <Link 
-                    href={link} 
-                    target={'_blank'}
-                >
-                    <p>公式サイト</p>
-                </Link>
-            </div>
+                <div className="flex gap-1">
+                    <Link 
+                        href={link} 
+                        target={'_blank'}
+                    >
+                        <p>Link</p>
+                    </Link>
+                </div>
             </CardFooter>
         </Card>
     );
